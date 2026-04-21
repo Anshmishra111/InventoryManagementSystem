600e17f95471d150c1bd116450685334467a4057
himanshu <anshmishra75436@gmail.com>
2026-04-21 15:05:14 +0530

    [Himanshu Mishra] Add. warehouse-service

---
+++ warehouse-service/src/main/java/com/bridgelabz/warehouseservice/repository/WarehouseRepository.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.warehouseservice.repository;
+
+import com.bridgelabz.warehouseservice.entity.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+    List<Warehouse> findByActiveTrue();
+}
