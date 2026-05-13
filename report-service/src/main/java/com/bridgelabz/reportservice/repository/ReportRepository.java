518b3306829e8c61ea7c98dc3bd6f86315303bd2
Himanshu Mishra <anshmishra111@gmail.com>
2026-05-13 11:37:21 +0530

    Isolate supplier-service - remove other service directories and .vscode

--- report-service/src/main/java/com/bridgelabz/reportservice/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.reportservice.repository;
-
-import com.bridgelabz.reportservice.entity.InventoryReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends JpaRepository<InventoryReport, Long> {
-}
