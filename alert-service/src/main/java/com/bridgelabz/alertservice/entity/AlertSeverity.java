833a616538cd6ccc479147cfc3af0a700d09db7c
Himanshu Mishra <anshmishra111@gmail.com>
2026-05-13 11:48:12 +0530

    Isolate purchase-service - fix and synchronize code

--- alert-service/src/main/java/com/bridgelabz/alertservice/entity/AlertSeverity.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.alertservice.entity;
-
-public enum AlertSeverity {
-    INFO,
-    WARNING,
-    CRITICAL
-}
--- alert-service/src/main/java/com/bridgelabz/alertservice/entity/AlertType.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.alertservice.entity;
-
-public enum AlertType {
-    LOW_STOCK,
-    OVERSTOCK,
-    PO_PENDING,
-    OVERDUE_RECEIPT,
-    SYSTEM
-}
